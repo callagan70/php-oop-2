89ace33b83b05417f5898ed96e2128bc3c5ea36c
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-12 16:13:06 +0100

    Grossi problemi

--- data/animal.php
+++ data/animal.php
@@ -1,16 +1,20 @@
 <?
-class product{ 
+class product{
     public $name;
     public $descrizione;
     public $price;
+    public $star;
+    // public $valutazione;
 
-    public function _construct ($_name, $_descrizione, $_price){
-        $this -> setName($_name);
-        $this -> setDescrizione($_descrizione);
-        $this -> setPrice($_price);
+    public function _construct ($_name, $_descrizione, $_price, $_star){
+        $this -> name -> $_name;
+        $this -> descrizione = $_descrizione;
+        $this -> price = $_price;
+        $this -> star = setStar();
+        // $this -> valutazione = setValutazione($star);
     }
-
-    public function setName(){
+//-------------------------------
+    public function setName($_name){
         $this -> name = $_name;
         return $this;
     }
@@ -18,46 +22,59 @@ class product{
     public function getName(){
         return $this -> name;
       }
-
-      public function setDescrizione(){
-        $this -> name = $_descrizione;
+//-------------------------------
+      public function setDescrizione($_descrizione){
+        $this -> descrizione = $_descrizione;
         return $this;
     }
 
     public function getDescrizione(){
         return $this -> descrizione;
       }
-      public function setPrice(){
-        $this -> name = $_price;
+//-------------------------------
+      public function setPrice($_price){
+        $this -> price = $_price;
         return $this;
     }
 
     public function getPrice(){
         return $this -> price;
       }
+//-------------------------------
+    public function setStar(){
+      $this -> star = rand(0, 5);
+      return $this;
+    }
+
+    public function getStar(){
+      return $this -> star;
+    }
+//-------------------------------
+    // public function setValutazione(){
+    //     if($star = 1){
+    //       $this -> valutazione ="Mediocre";
+    //     }elseif ($star = 2){
+    //       $this -> valutazione ="Media";
+    //     }elseif($star = 3){
+    //       $this -> valutazione ="Buona";
+    //     }elseif($star = 4){
+    //       $this -> valutazione ="Ottima";
+    //     }else{
+    //       $this -> valutazione ="eccellente";
+    //     }
+
+    //   return $this;
+    // }
+
+    // public function getValutazione(){
+    //   return $this -> valutazione;
+    // }
+//-------------------------------
+
+$monge = new product ("Monge", "Pappa cani", 58);
+$purina = new product ("Purina", "Pappa gatti", 68);
 
 
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+}?>
 
 
--- index.php
+++ index.php
@@ -1,10 +1,37 @@
 <?php 
 
-require __DIR__ . '/data/animal.php'
+require __DIR__ . '/data/animal.php';
 
 
+var_dump($monge);
+var_dump($purina);
 
-
+$monge -> setStar();
+// $monge -> setValutazione($monge -> $star);
 
 ?>
 
+<!-- L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+BONUS:
+Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto). -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p> Stelle: <?php echo $monge -> getStar() ?> </p>
+    <p>Nome: <?= $monge -> name?></p>
+    <!-- <p> Valutazione: <?php echo $monge -> getValutazione() ?> </p> -->
+    
+</body>
+</html>
